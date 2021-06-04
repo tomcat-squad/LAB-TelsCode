570c05993a29108877f8ab4c8337a14f93af40fc
mchevro <mchevro19@gmail.com>
2021-06-04 12:03:10 +0700

    New Index Blind SQL

--- WEB APPLICATION/LAB - SQL INJECTION/BLIND SQL INJECTION/index.php
+++ WEB APPLICATION/LAB - SQL INJECTION/BLIND SQL INJECTION/index.php
@@ -1,16 +1,7 @@
 <?php
-   include('assets/config/koneksi.php');
+   include_once '../../header.php';
+   include_once 'assets/config/koneksi.php';
 ?>
-<!doctype html>
-<html lang="en">
-   <head>
-      <!-- Required meta tags -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- Bootstrap CSS -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-      <title>SEKOLAH HACKER - TOMCAT SQUAD</title>
-   </head>
    <body>
       <div class="container">
          <div class="text-center">
@@ -18,7 +9,7 @@
          </div>
          <?php 
             if(isset($_GET['cari'])){
-               $cari = $_GET['cari']; 
+               $cari = $_GET['cari'];
                echo "<b>Hasil pencarian : ".$cari."</b>";
             }
          ?>
@@ -26,17 +17,17 @@
          <form action="index.php" method="get">
                <label>Cari Data Member</label>
                <input class="form-control" type="text" name="cari">
-               <input type="submit" value="Cari">
+               <input class="btn btn-dark" style="margin-top: 10px; margin-bottom: 10px;" type="submit" value="Cari">
             </form>
          <table class="table table-bordered">
             <tr>
-               <th>No</th>
+               <th>ID</th>
                <th>Nama</th>
             </tr>
             <?php 
                if(isset($_GET['cari'])){
                   $cari = $_GET['cari']; 
-                  $data = mysqli_query($koneksi, "SELECT * FROM peserta WHERE nama LIKE '%".$cari."%'");	
+                  $data = mysqli_query($koneksi, "SELECT * FROM peserta WHERE id = {$cari}");	
                }else{
                   $data = mysqli_query($koneksi, "SELECT * FROM peserta");		
                }
@@ -52,4 +43,6 @@
          </div>
       </div>
    </body>
-</html>
+<?php
+   include_once '../../footer.php';
+?>
