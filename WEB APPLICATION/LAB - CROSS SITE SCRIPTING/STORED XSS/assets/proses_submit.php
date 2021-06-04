b75afb23acd80ee3a3d570ee63046eefe74ea083
mchevro <mchevro19@gmail.com>
2021-06-04 11:16:05 +0700

    Script Patch Stored XSS

--- WEB APPLICATION/LAB - CROSS SITE SCRIPTING/STORED XSS/assets/proses_submit.php
+++ WEB APPLICATION/LAB - CROSS SITE SCRIPTING/STORED XSS/assets/proses_submit.php
@@ -3,10 +3,18 @@
     include_once('../assets/config/koneksi.php');
 
     // Menangkap data yang dikirimkan dari form
+    // SEBELUM DIPATCH
     $nama = $_POST['nama'];
     $kelas = $_POST['kelas'];
     $umur = $_POST['umur'];
 
+    /* 
+    // SESUDAH DI PATCH
+    $nama = strip_tags($_POST['nama']);
+    $kelas = strip_tags($_POST['kelas']);
+    $umur = strip_tags($_POST['umur']);
+    */
+    
     // Memasukan data ke database
     mysqli_query($koneksi, "INSERT INTO peserta (nama, kelas, umur) VALUES ('$nama', '$kelas', '$umur')");
     
